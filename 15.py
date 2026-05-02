5d56a040f855fd4af3b2c5be38847b00ea00a9b9
vinay.t25gcu <vinayht2007@gmail.com>
2026-05-02 11:45:54 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,10 @@
+print("Prime numbers between 1 and 20 are:")
+ulmt=20;
+for num in range(ulmt):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
